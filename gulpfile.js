540f50682436b8278816361ba0f17eabafefe8a5
Andrei Gunderin <ga@web-notes.info>
2022-06-29 04:19:06 +0300

    chore: step 1

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,19 @@
 const { src, dest, parallel, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const cleanCSS = require('gulp-clean-css');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
 
 const buildSass = () => src('app/scss/*.scss')
 	.pipe(sass({
+		includePaths: ["node_modules/bootstrap/scss/"],
 		sourceMap: false,
 		outputStyle: 'compressed',
+	}))
+	.pipe(cleanCSS({
+		level: {
+			1: { specialComments: false }
+		}
 	}))
 	.pipe(dest('build/css/'))
 	.pipe(browserSync.stream());
@@ -18,8 +25,8 @@ const buildPug = () => src('app/*.pug')
 
 const browserSyncJob = () => {
 	browserSync.init({ server: 'build/' });
-	watch('app/scss/*.scss', buildSass);
-	watch('app/*.pug', buildPug);
+	watch('app/scss/**/*.scss', buildSass);
+	watch('app/**/*.pug', buildPug);
 };
 
 const development =() => {
