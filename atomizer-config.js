e9ce6285f4b1e9c36929dd4b643d48386b709bf7
Andrei Gunderin <ga@web-notes.info>
2022-07-02 04:19:56 +0300

    chore: step 2-3

---
+++ atomizer-config.js
@@ -0,0 +1,30 @@
+module.exports = {
+	'breakPoints': {
+		'xs': '@media (max-width:576px)',
+		'sm': '@media (min-width:576px)',
+		'md': '@media (min-width:768px)',
+		'lg': '@media (min-width:992px)',
+		'xl': '@media (min-width:1200px)',
+		'xxl': '@media (min-width:1600px)'
+	},
+	'custom': {
+		// FONTS
+		roboto: 'Roboto, Verdana, sans-serif',
+
+		// BACKGROUND IMAGES & GRADIENTS
+
+		// COLORS
+		grayColor: '#EFEFEF',
+		primaryColor: '#4545A5',
+		secondaryColor: '#6C757D',
+		successColor: '#28A745',
+		infoColor: '#17A2B8',
+		warningColor: '#FFC107',
+		dangerColor: '#DC3545',
+		lightColor: '#FBFBFB',
+		darkColor: '#343A40',
+
+		// OTHER VALUES
+		bodyHeightXl: 'calc(100vh - 3rem)'
+	},
+};
--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,15 @@
-const { src, dest, parallel, watch } = require('gulp');
+const { src, dest, parallel, series, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const cleanCSS = require('gulp-clean-css');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const atomizer = require('gulp-atomizer');
+const atomizerConfig = require('./atomizer-config.js');
 
 const buildSass = () => src('app/scss/*.scss')
 	.pipe(sass({
-		includePaths: ["node_modules/bootstrap/scss/"],
+		includePaths: ['node_modules/normalize.css/'],
 		sourceMap: false,
 		outputStyle: 'compressed',
-	}))
-	.pipe(cleanCSS({
-		level: {
-			1: { specialComments: false }
-		}
 	}))
 	.pipe(dest('build/css/'))
 	.pipe(browserSync.stream());
@@ -23,18 +19,26 @@ const buildPug = () => src(['app/pug/**/*.pug', '!app/pug/**/_*.pug'])
 	.pipe(dest('build/'))
 	.pipe(browserSync.stream());
 
+const atomicCss = () => src('build/**/*.html')
+	.pipe(atomizer({
+		acssConfig: atomizerConfig
+	}))
+	.pipe(dest('build/css/'))
+	.pipe(browserSync.stream());
+
 const browserSyncJob = () => {
 	browserSync.init({ server: 'build/' });
 	watch('app/scss/**/*.scss', buildSass);
-	watch('app/**/*.pug', buildPug);
+	watch('app/**/*.pug', series(buildPug, atomicCss));
 };
 
 const development =() => {
 	buildSass();
 	buildPug();
+	atomicCss();
 	browserSyncJob();
 };
 
 exports.server = browserSyncJob;
-exports.build = parallel(buildSass, buildPug);
+exports.build = parallel(buildSass, series(buildPug, atomicCss));
 exports.dev = development;
