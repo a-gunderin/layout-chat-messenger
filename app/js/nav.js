24c29570569b7fdc6169d2d592018baeaf323fc6
Andrei Gunderin <ga@web-notes.info>
2022-07-10 04:14:20 +0300

    chore: step 4

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,12 +1,20 @@
 const navEl = document.getElementById('navigation');
-const navLinksEls = navEl.querySelectorAll('[data-tab-link]');
-const tabsEls = document.querySelectorAll('#tabs > div');
+const navLinkEls = navEl.querySelectorAll('[data-tab-link]');
+const tabEls = document.querySelectorAll('#tabs > div');
+const tabIds = Array.from(tabEls).map((i) => i.id);
 
-navLinksEls.forEach((navLink) => {
+navLinkEls.forEach((navLink) => {
 	navLink.addEventListener('click', (e) => {
-		e.preventDefault();
-		tabsEls.forEach((tab) => tab.classList.add('D(n)'));
+		tabEls.forEach((tab) => tab.classList.add('D(n)'));
 		const activeId = e.target.closest('a').dataset.tabLink;
 		document.getElementById(activeId).classList.remove('D(n)');
 	})
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+	const hash = window.location.hash;
+	if (tabIds.includes(hash.substring(1))) {
+		tabEls.forEach((tab) => tab.classList.add('D(n)'));
+		document.querySelector(hash).classList.remove('D(n)');
+	}
 });
